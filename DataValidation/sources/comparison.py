2ae9acb3c586597f300c4d542d28044751f2d2ef
Zach Rieck <zrr817@gmail.com>
2023-01-31 11:27:38 -0700

    Finished argparse migration

--- DataValidation/sources/comparison.py
+++ DataValidation/sources/comparison.py
@@ -18,6 +18,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import sources
 from compare_reports import Comparison
+from args import args
 
 configs = json.load(open('./sources/json_sources/config.json'))
 
@@ -902,26 +903,6 @@ def match_names(edw2_ro, edw3_ro):
         raise Exception('The length of the edw2 column names does not match edw3')
 
 def main():
-    # Define comparison column name and join_on vars here
-    # Do this via argparse
-    global args
-    parser = argparse.ArgumentParser("Run regression over Avantlink Dashboard Reports by report category")
-    parser.add_argument('-p', '--period', type=str, help="'last year', 'last quarter', or 'last month'", required=False)
-    parser.add_argument('-m', '--manual', action='store_true',
-                        help="Run in manual mode if you want to insert your request objects manually", required=False)
-    parser.add_argument('-ra', '--run-all', action='store_true')
-    parser.add_argument('-s', '--sim', type=str, help="i.e 'kiran_dev', 'adam_dev', 'le_dev'")
-    parser.add_argument('-j', '--join', type=str,
-                        help="the name of the colum(s) to perform the join on. If multiple cols - use ',' to separate the cols.")
-    parser.add_argument('-r', '--remove', type=str,
-                        help="the name of the colum(s) to remove. If multiple cols - use ',' to separate the cols.")  # Todo: Add Drop Columns Functionality
-    parser.add_argument('-d', '--diffs', action='store_true')  # Todo: add show only rows that are different
-    parser.add_argument('-c', '--comparison-column', type=str,
-                        help="The name of the column to be compared (must be an int or float)")
-    parser.add_argument('-sd', '--start-date', type=str, help="The Start Date - Format == mm_dd_yyyy")
-    parser.add_argument('-ed', '--end-date', type=str, help="The End Date - Format === mm_dd_yyyy")
-    parser.add_argument('-mer', '--merchant', type=str, help="The merchant uuid or merchant name")
-    args = parser.parse_args()
     # Instantiate the class
     cascade = Cascade()
     cascade.semaphore_count = 3
