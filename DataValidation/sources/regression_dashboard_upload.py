9043df21bee7f9946fc589d8582088f70c09d0de
codeblackwell <codeblackwell@gmail.com>
2023-08-03 12:23:29 -0700

    DRY up the code

--- DataValidation/sources/regression_dashboard_upload.py
+++ DataValidation/sources/regression_dashboard_upload.py
@@ -28,7 +28,6 @@ class UpdateDashboardLog:
         "ta": [],
         "tw": []
     }
-
     avantlog_spreadsheet_ids = {
         "ta": {
             "fact_postgres": "1VJkxttwbiVLfip04xICyDlFoBQy5Mf_HizP9hzvjtss",
@@ -45,11 +44,11 @@ class UpdateDashboardLog:
     }
     merchant_summary_RANGE = {
         "rei.com": "Merchant Summary!C2:C12",
-        "Black Diamond": "Merchant Summary!A3",
-        "Carousel Checks": "Merchant Summary!A4",
-        "Palmetto State Armory": "Merchant Summary!A5",
-        "RTIC Outdoors": "Merchant Summary!A6",
-        "A_Life_Plus": "Merchant Summary!A7"
+        "Black Diamond": "Merchant Summary!B2:B12",
+        "Carousel Checks": "Merchant Summary!D2:D12",
+        "Palmetto State Armory": "Merchant Summary!E2:E12",
+        "RTIC Outdoors": "Merchant Summary!F2:F12",
+        "A_Life_Plus": "Merchant Summary!G2:G12"
     }
     categorical_report_RANGE = {
         "rei.com": "REI.com!E1:Z"
@@ -62,19 +61,74 @@ class UpdateDashboardLog:
         "RTIC Outdoors": "Test Accounts Overview!A6",
         "A_Life_Plus": "Test Accounts Overview!A7"
     }
-
-    all_sources_test_account_range = {
+    all_sources_merchant_summary_RANGE = {
         "trending_widget": {
-            "cube_postgres": {},
-            "fact_postgres": {},
-            "fact_redshift": {},
-            "cube_olap": {}
+            "cube_postgres": {
+                "Black Diamond": "Merchant Summary!B2:B12",
+                "REI.com": "Merchant Summary!C2:C12",
+                "Carousel Checks": "Merchant Summary!D2:D12",
+                "Palmetto State Armory": "Merchant Summary!E2:E12",
+                "RTIC Outdoors": "Merchant Summary!F2:F12",
+                "A_Life_Plus": "Merchant Summary!G2:G12"
+            },
+            "fact_postgres": {
+                "Black Diamond": "Merchant Summary!B15:B25",
+                "REI.com": "Merchant Summary!C15:C25",
+                "Carousel Checks": "Merchant Summary!D15:D25",
+                "Palmetto State Armory": "Merchant Summary!E15:E25",
+                "RTIC Outdoors": "Merchant Summary!F15:F25",
+                "A_Life_Plus": "Merchant Summary!G15:G25"
+            },
+            "fact_redshift": {
+                "Black Diamond": "Merchant Summary!B28:B38",
+                "REI.com": "Merchant Summary!C28:C38",
+                "Carousel Checks": "Merchant Summary!D28:D38",
+                "Palmetto State Armory": "Merchant Summary!E28:E38",
+                "RTIC Outdoors": "Merchant Summary!F28:F38",
+                "A_Life_Plus": "Merchant Summary!28:G38G"
+            },
+            "cube_olap": {
+                "Black Diamond": "Merchant Summary!B41:B51",
+                "REI.com": "Merchant Summary!C41:C51",
+                "Carousel Checks": "Merchant Summary!D41:D51",
+                "Palmetto State Armory": "Merchant Summary!E41:E51",
+                "RTIC Outdoors": "Merchant Summary!F41:F51",
+                "A_Life_Plus": "Merchant Summary!G41:G51"
+            }
         },
         "top_affiliates_widget": {
-            "cube_postgres": {},
-            "fact_postgres": {},
-            "fact_redshift": {},
-            "cube_olap": {}
+            "cube_postgres": {
+                "Black Diamond": "Merchant Summary!I2:I12",
+                "REI.com": "Merchant Summary!J2:J12",
+                "Carousel Checks": "Merchant Summary!K2:K12",
+                "Palmetto State Armory": "Merchant Summary!L2:L12",
+                "RTIC Outdoors": "Merchant Summary!M2:M12",
+                "A_Life_Plus": "Merchant Summary!N2:N12"
+            },
+            "fact_postgres": {
+                "Black Diamond": "Merchant Summary!I15:I25",
+                "REI.com": "Merchant Summary!J15:J25",
+                "Carousel Checks": "Merchant Summary!K15:K25",
+                "Palmetto State Armory": "Merchant Summary!L15:L25",
+                "RTIC Outdoors": "Merchant Summary!M15:M25",
+                "A_Life_Plus": "Merchant Summary!N15:N25"
+            },
+            "fact_redshift": {
+                "Black Diamond": "Merchant Summary!I28:I38",
+                "REI.com": "Merchant Summary!J28:J38",
+                "Carousel Checks": "Merchant Summary!K28:K38",
+                "Palmetto State Armory": "Merchant Summary!L28:L38",
+                "RTIC Outdoors": "Merchant Summary!M28:M38",
+                "A_Life_Plus": "Merchant Summary!N8:N38G"
+            },
+            "cube_olap": {
+                "Black Diamond": "Merchant Summary!I41:I51",
+                "REI.com": "Merchant Summary!J41:J51",
+                "Carousel Checks": "Merchant Summary!K41:K51",
+                "Palmetto State Armory": "Merchant Summary!L41:L51",
+                "RTIC Outdoors": "Merchant Summary!M41:M51",
+                "A_Life_Plus": "Merchant Summary!N41:N51"
+            }
         }
     }
 
@@ -116,89 +170,89 @@ class UpdateDashboardLog:
         # self.update_test_account_overviews()
 
     def update_all_sources_merchant_summary(self):
-        pass
+        tw_merchant_account_overview_body = {'values': self.merchant_summary_report_values["tw"]}
+        try:
+            # upload - Merchant Summary Report - All Sources - Trending Widget
+            tw_merchant_account_overview = self.sheet.update(
+                spreadsheetId=self.avantlog_spreadsheet_ids["all_sources"],
+                range=self.all_sources_merchant_summary_RANGE["trending_widget"][self.sql_source][self.merchant_name],
+                valueInputOption=VALUE_INPUT_OPTION,
+                body=tw_merchant_account_overview_body
+            ).execute()
+            # print(tw_merchant_account_overview)
+        except HttpError as err:
+            print(tw_merchant_account_overview, err)
+
+        ta_merchant_account_overview_body = {'values': self.merchant_summary_report_values["ta"]}
+        try:
+            # upload - Merchant Summary Report - All Sources - Top Affiliates Widget
+            ta_merchant_account_overview = self.sheet.update(
+                spreadsheetId=self.avantlog_spreadsheet_ids["all_sources"],
+                range=self.all_sources_merchant_summary_RANGE["top_affiliates_widget"][self.sql_source][
+                    self.merchant_name],
+                valueInputOption=VALUE_INPUT_OPTION,
+                body=ta_merchant_account_overview_body
+            ).execute()
+            # print(ta_merchant_account_overview)
+        except HttpError as err:
+            print(ta_merchant_account_overview, err)
 
     def update_merchant_summaries(self):
-        # self.update_ta_merchant_summary()
-        self.update_tw_merchant_summary()
+        self.update_merchant_summary("trending_widget")
+        self.update_merchant_summary("top_affiliates_widget")
 
     def update_categorical_reports(self):
-        self.update_ta_merchant_categorical_report()
-        self.update_tw_merchant_categorical_report()
+        self.update_merchant_categorical_report("trending_widget")
+        self.update_merchant_categorical_report("top_affiliates_widget")
 
     def update_test_account_overviews(self):
-        # self.update_ta_test_account_overview()
-        self.update_tw_test_account_overview()
+        self.update_test_account_overview("trending_widget")
+        self.update_test_account_overview("top_affiliates_widget")
 
-    def update_ta_test_account_overview(self):
-        value_range_body = {
-            "values": [self.test_account_overview["Last Test"]]
-        }
-        request = self.sheet.update(
-            spreadsheetId=self.avantlog_spreadsheet_ids["ta"][self.sql_source],
-            range=self.test_account_overview_range[self.merchant_name],
-            valueInputOption=VALUE_INPUT_OPTION,
-            body=value_range_body
-        )
-        response = request.execute()
-        pprint(response)
-
-    def update_tw_test_account_overview(self):
-
-        account_overview_body = {'values': [[
-            f"{self.test_account_overview['Last Test']} \n {self.test_account_overview['pass/fail']['trending_widget']}"]]}
+    def update_test_account_overview(self, widget):
+        widget_marker = self.get_widget_marker(widget)
+        merchant_account_overview_body = {'values': [[
+            f"{self.test_account_overview['Last Test']} \n {self.test_account_overview['pass/fail'][widget]}"]]}
         try:
             test_account_overview = self.sheet.update(
-                spreadsheetId=self.avantlog_spreadsheet_ids["tw"][self.sql_source],
+                spreadsheetId=self.avantlog_spreadsheet_ids[widget_marker][self.sql_source],
                 range=self.test_account_overview_range[self.merchant_name],
                 valueInputOption=VALUE_INPUT_OPTION,
-                body=account_overview_body
+                body=merchant_account_overview_body
             ).execute()
             pprint(test_account_overview)
         except HttpError as err:
+            pprint(test_account_overview)
             print(err)
 
-    def update_ta_merchant_summary(self):
-        account_overview_body = {'values': self.merchant_summary_report_values["ta"]}
+    def update_merchant_summary(self, widget):
+        widget_marker = self.get_widget_marker(widget)
+        account_overview_body = {'values': self.merchant_summary_report_values[widget_marker]}
         try:
-            merchant_account_overview = self.sheet.append(
-                spreadsheetId=self.avantlog_spreadsheet_ids["ta"][self.sql_source],
+            # upload - Merchant Summary Report - Trending Widget
+            merchant_account_overview = self.sheet.update(
+                spreadsheetId=self.avantlog_spreadsheet_ids[widget_marker][self.sql_source],
                 range=self.merchant_summary_RANGE[self.merchant_name.lower()],
                 valueInputOption=VALUE_INPUT_OPTION,
                 body=account_overview_body
             ).execute()
-            print(
-                f"{(merchant_account_overview.get('updates').get('updatedCells'))} cells appended. - Top accounts - Overview")
+            pprint(merchant_account_overview)
         except HttpError as err:
             print(err)
 
-    def update_tw_merchant_summary(self):
-        account_overview_body = {'values': self.merchant_summary_report_values["tw"]}
-        try:
-            # upload Test Accounts Overview Report - Trending Widget
-            merchant_account_overview = self.sheet.append(
-                spreadsheetId=self.avantlog_spreadsheet_ids["tw"][self.sql_source],
-                range=self.merchant_summary_RANGE[self.merchant_name.lower()],
-                valueInputOption=VALUE_INPUT_OPTION,
-                body=account_overview_body
-            ).execute()
-            print(
-                f"{(merchant_account_overview.get('updates').get('updatedCells'))} cells appended. - Trending Widget - Overview")
-        except HttpError as err:
-            print(err)
-
-    def update_ta_merchant_categorical_report(self):
-        top_accounts_categorical_report_body = {'values': self.categorical_report_values["top_affiliates_widget"]}
-        # upload Categorical Report for each Widget -- Top Accounts / Top Affiliates
+    def update_merchant_categorical_report(self, widget):
+        widget_marker = self.get_widget_marker(widget)
+        top_accounts_categorical_report_body = {'values': self.categorical_report_values[widget]}
+        # upload Categorical Report for each Widget -- Top Affiliates / Trending widget
         try:
             top_accounts_result = self.sheet.append(
-                spreadsheetId=self.avantlog_spreadsheet_ids["ta"][self.sql_source],
+                spreadsheetId=self.avantlog_spreadsheet_ids[widget_marker][self.sql_source],
                 range=self.categorical_report_RANGE[self.merchant_name.lower()],
                 valueInputOption=VALUE_INPUT_OPTION,
                 body=top_accounts_categorical_report_body
             ).execute()
             print(
-                f"{(top_accounts_result.get('updates').get('updatedCells'))} cells appended. - Top Accounts - Categorical")
+                f"{(top_accounts_result.get('updates').get('updatedCells'))} cells appended. - {widget} - Categorical")
         except HttpError as err:
             print(err)
 
@@ -217,11 +271,11 @@ class UpdateDashboardLog:
         except HttpError as err:
             print(err)
 
-    def process_account_overview_report_for_google(self, subject=None):
+    def process_account_overview_report_for_google(self):
         result = []
         category_order = ["Sales", "Combined Commission", "Network Commission",
                           "Clicks % Impressions", "Adjustments", "Affiliate Commission"]
-        subject = subject or self.merchant_summary
+        subject = self.merchant_summary
         result.append([subject["Last Test"]])
         result.append([subject["Date Range"]])
         result.append([subject["Merchant"]])
@@ -236,13 +290,13 @@ class UpdateDashboardLog:
         for sublist in ta_result:
             for val in sublist:
                 new_val = str(val).replace("{", "").replace("}", "").replace(",", "\n").replace("]", "")
-            sublist.pop()
-            sublist.append(new_val)
+                sublist.pop()
+                sublist.append(new_val)
         for sublist in tw_result:
             for val in sublist:
                 new_val = str(val).replace("{", "").replace("}", "").replace(",", "\n").replace("]", "")
-            sublist.pop()
-            sublist.append(new_val)
+                sublist.pop()
+                sublist.append(new_val)
         self.merchant_summary_report_values["ta"] = ta_result
         self.merchant_summary_report_values["tw"] = tw_result
         return tw_result, ta_result
@@ -257,3 +311,10 @@ class UpdateDashboardLog:
                     sublist[0] = "PASS!"
         self.merchant_summary_report_values = new_summary_values
         return new_summary_values
+
+    @staticmethod
+    def get_widget_marker(widget):
+        if widget == "top_affiliates_widget":
+            return "ta"
+        elif widget == "trending_widget":
+            return "tw"
