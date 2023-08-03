0823f64998cc7dbe0711247e95d63752c00eeab6
Ubuntu <ubuntu@ip-172-31-64-192.ec2.internal>
2023-08-03 18:46:46 +0000

    Reduced timeout

--- DataValidation/sources/comparison.py
+++ DataValidation/sources/comparison.py
@@ -414,8 +414,9 @@ class Cascade:
             comparison.set_outputs(simple_report_name=self.report_name,
                                 simple_difference=simple_difference_options,
                                 dashboard_regression=dashboard_regression)
+             
             try:
-                await asyncio.wait_for(comparison.run_and_barf(), timeout=3000000)
+                await asyncio.wait_for(comparison.run_and_barf(), timeout=60)
             except asyncio.TimeoutError as e:
                 self.errors.append({
                     "error": "timeout",
